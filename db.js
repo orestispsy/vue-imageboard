b9e9351aeaf21120c669c984d2cf18376a44f0bf
Orestis Psycharis <opsychar@gmai.com>
2021-02-26 09:53:54 +0100

    part 1

---
+++ db.js
@@ -0,0 +1,13 @@
+const spicedPg = require("spiced-pg");
+
+const db = spicedPg(
+        "postgres:postgres:postgres@localhost:5432/imageboard"
+);
+
+
+module.exports.getAllImages = () => {
+    const q = `
+        SELECT * FROM images;
+    `;
+    return db.query(q);
+};
