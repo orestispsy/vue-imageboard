d2a1547b97fb255c48fddc7c66b1b9f64ed35ed5
Orestis Psycharis <opsychar@gmai.com>
2021-02-27 20:35:03 +0100

    testing online

--- db.js
+++ db.js
@@ -1,13 +1,24 @@
 const spicedPg = require("spiced-pg");
 
 const db = spicedPg(
+    process.env.DATABASE_URL ||
         "postgres:postgres:postgres@localhost:5432/imageboard"
 );
-
 
 module.exports.getAllImages = () => {
     const q = `
         SELECT * FROM images;
     `;
     return db.query(q);
+};
+
+module.exports.addImage = (url, username, title, description) => {
+    const q = `
+        INSERT INTO images (url, username, title, description)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, url, username, title, description
+    `;
+    const params = [url, username, title, description];
+    console.log("q: ", q);
+    return db.query(q, params);
 };
