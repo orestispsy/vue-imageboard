0be979fd57a6a98f83d3a8463edbd7ec77fae5e0
Orestis Psycharis <opsychar@gmai.com>
2021-03-04 09:48:57 +0100

    imageboards v5.0

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ module.exports.getAllImages = () => {
     const q = `
         SELECT * FROM images
         ORDER BY id DESC
+        LIMIT 6
     `;
     return db.query(q);
 };
@@ -35,17 +36,35 @@ module.exports.getSelectedImage = (id) => {
 module.exports.getSelectedComments = (id) => {
     const q = `
         SELECT * FROM comments WHERE img_id = $1
+        ORDER BY id DESC
     `;
     const params = [id];
     return db.query(q, params);
 };
-``;
+
 module.exports.addComment = (username, img_id, comment) => {
     const q = `
         INSERT INTO comments (username, img_id, comment)
         VALUES ($1, $2, $3)
+        RETURNING id, username, img_id, comment, created_at
     `;
     const params = [username, img_id, comment];
     console.log("q: ", q);
     return db.query(q, params);
 };
+
+module.exports.getNextRes = (id) => {
+    const q = `
+        SELECT url, title, id, (
+        SELECT id FROM images
+        ORDER BY id ASC
+        LIMIT 1
+        ) AS "lowestId" FROM images
+        WHERE id < $1
+        ORDER BY id DESC
+        LIMIT 6;
+    `;
+    const params = [id];
+    return db.query(q, params);
+};
+
